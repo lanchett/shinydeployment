472d39412ef2ff6573d1999545b8d672ae55830b
Thomas Langeland <thomas.langeland@pwc.com>
2020-01-10 18:04:45 +0100

    Adding images and functionality for choosing drinks

--- app/server.R
+++ app/server.R
@@ -1,11 +1,50 @@
 library(shiny)
 
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
-  
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+server <- function(input, output, session) {
+  ntext <- eventReactive(input$goButton, {
+    saveRDS(data.frame(
+        Drink = input$Drink, Navn = input$Navn
+      ),
+      file = paste0(
+        here::here(),
+        "/",
+        stringi::stri_rand_strings(1, 100),
+        ".rds"
+      )
+    )
+    
+    list(
+      input = input$Drink,
+      files = files <-
+        list.files(here::here(), pattern = ".rds")
+    )
   })
+  
+  output$nText <- renderText({
+    paste(ntext()$input, "er bestilt")
+  })
+  
+  output$orders_total <- renderText({
+    paste("Totalt antall bestillinger idag:", length(ntext()$files))
+  })
+  
+  
+  data <- eventReactive(input$sebestillinger,{
+    files <-
+      list.files(here::here(), pattern = ".rds")
+    files <-  paste0(
+      here::here(),"/", files)
+    
+    if(is.null(files)){
+      return()
+    }
+    bestillinger <- purrr::map_df(files, ~ readRDS(.x))
+    df <- bestillinger
+    df
+  })
+  
+  output$summary_table <- renderDataTable({
+    data()
+  })
+  
 }
--- app/ui.R
+++ app/ui.R
@@ -1,31 +1,70 @@
-#
-# This ShinyApp shows how to evaluate and compare
-# the quality of linear models.
-#
 
 # Use ShinyDashboard
 library(shinydashboard)
+library(shiny)
+library(DT)
+library(dplyr)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Done", tabName = "Done", icon = icon("th")),
-      menuItem("Dtwo", tabName = "Dtwo", icon = icon("th"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "Done",
-              h2("Drink Two")
-      ),
+  dashboardHeader(title = "Drinkathon Dashboard"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Drinkmeny", tabName = "Drinkmeny", icon = icon("th")),
+    menuItem("Bestillinger", tabName = "Bestillinger", icon = icon("th"))
+  )),
+  dashboardBody(tabItems(
+    tabItem(
+      tabName = "Drinkmeny",
+      h2("Finn noe du liker"),
       
-      # Second tab content
-      tabItem(tabName = "Dtwo",
-              h2("Drink Two")
-      )
+      fluidRow(box(title = "GT",
+                   tags$a(a(
+                     img(src = 'GT.jpg')
+                   ))),
+      box(title = "Mojito",
+                   tags$a(a(
+                     img(src = 'Mojito.jpg')
+                   ))),
+      box(title = "Whisky Sour",
+                   tags$a(a(
+                     img(src = 'Whisky.jpg')
+                   ))),
+      box(title = "Aloe Vera",
+          tags$a(a(
+            img(src = 'Aloe_Vera.jpg')
+          ))),
+      box(title = "Knickerbocker",
+          tags$a(a(
+            img(src = 'Knickerbocker.jpg')
+          ))),
+      box(title = "Sure Føtter",
+          tags$a(a(
+            img(src = 'Sure_fotter.jpg')
+          ))))
+    ),
+    
+    tabItem(
+      tabName = "Bestillinger",
+      selectInput(
+        "Drink",
+        "Drink:",
+        c(
+          "GT" = "GT",
+          "Mojito" = "Mojito",
+          "Whisky Sour" = "Whisky Sour",
+          "Sure Føtter" = "Sure Føtter",
+          "Knickerbocker" = "Knickerbocker",
+          "Aloe Vera" = "Aloe Vera"
+          
+        )
+      ),
+      textInput("Navn", "Skriv ditt navn"),
+      br(),
+      actionButton("goButton", "Bestill nå"),
+      verbatimTextOutput("nText"),
+      verbatimTextOutput("orders_total"),
+      hr(),
+      actionButton("sebestillinger","Se bestillingsoversikt"),
+      dataTableOutput("summary_table")
     )
-  )
+  ))
 )
-
